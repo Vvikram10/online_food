039fc9ccda452ffe7a125857dee4d3a6eb95e209
Vikram <165460701+Vvikram10@users.noreply.github.com>
2024-11-13 13:27:49 +0530

    Update settings.py

--- online_food/settings.py
+++ online_food/settings.py
@@ -19,11 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lll&dsvbzh^2f-@d7t(4@68o*5s+s!6ym4%+sdn=lcm6fzr!&t'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
