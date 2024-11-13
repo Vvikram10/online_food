c9c08cea7ec9946c3a7da948368d6bca651435a6
Vvikram10 <vvikram3312@gmail.com>
2024-11-13 13:41:56 +0530

    first commit

--- online_food/settings.py
+++ online_food/settings.py
@@ -16,17 +16,8 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-<<<<<<< HEAD
-
-
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-=======
->>>>>>> 039fc9ccda452ffe7a125857dee4d3a6eb95e209
 
 ALLOWED_HOSTS = []
 
